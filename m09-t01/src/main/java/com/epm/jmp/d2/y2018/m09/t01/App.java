426a3a2d4ab4896d3afa8f5a187fec53c437a751
Igar Kanavalau <ikonovi@gmail.com>
2020-06-24 00:38:16 +0300

    init

---
+++ m09-t01/src/main/java/com/epm/jmp/d2/y2018/m09/t01/App.java
@@ -0,0 +1,27 @@
+package com.epm.jmp.d2.y2018.m09.t01;
+
+import com.epam.jmp.d2.y2018.m09.t01.service.RaceService;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class App {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ConfigurableApplicationContext ctx =
+                new ClassPathXmlApplicationContext("spring-context-all.xml");
+
+        RaceService raceService = (RaceService) ctx.getBean(RaceService.class);
+
+        raceService.showRaceInfo();
+        raceService.offerToChooseHorse();
+        raceService.startRace();
+        raceService.announceWinner();
+
+        ctx.close();
+    }
+
+
+}
